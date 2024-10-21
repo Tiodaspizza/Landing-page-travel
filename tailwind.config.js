25ed9465f36712c3152c53dbb103dd03a010a51e
Matheus Novaes <149258757+Tiodaspizza@users.noreply.github.com>
2024-10-21 12:42:06 -0300

    updates

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'primaria': '#2E476B', 
+        'secundaria': '#F66F4D', 
+        'background': '#FAF8ED',
+      },
+    },
+  },
+  plugins: [],
+}
